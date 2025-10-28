da5d6638f82eba150fc7f48a05e9aa74daf0d72d
iordix <yordir932@gmail.com>
2025-10-28 16:51:58 -0600

    Feat
    Creó Controlador de Login

---
+++ Gestor de Proyectos Académicos/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gestor_de_Proyectos_Académicos.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
